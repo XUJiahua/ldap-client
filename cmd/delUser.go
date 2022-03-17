7b1a1564788f89c12d33e4fde130775c30b96914
jiahua <jiahua.xu@outlook.com>
2022-03-17 16:20:41 +0800

    feat: cmd group add/del user

---
+++ cmd/delUser.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/xujiahua/ldap-client/pkg/ldap"
+)
+
+// delUserCmd represents the delUser command
+var delUserCmd = &cobra.Command{
+	Use:   "delUser",
+	Short: "remove a user from a group, user email and group name are required",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 2 {
+			fmt.Println("group name and user email are required")
+			os.Exit(1)
+		}
+		client, err := ldap.NewClient(ldapURL, ldapAdminUser, ldapAdminPassword, peopleDN, groupDN)
+		handleErr(err)
+		err = client.RemoveUserFromGroupEasy(args[0], args[1])
+		handleErr(err)
+		fmt.Printf("%s removed from group %s\n", args[0], args[1])
+	},
+}
+
+func init() {
+	groupCmd.AddCommand(delUserCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// delUserCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// delUserCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
